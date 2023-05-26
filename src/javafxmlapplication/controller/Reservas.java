44e4f85e59aa5bfbd2da626d5d407febc459fec5
marcramisg <marcramisg@gmail.com>
2023-05-26 14:38:02 +0200

    problemas en mis reservas

--- src/javafxmlapplication/controller/Reservas.java
+++ src/javafxmlapplication/controller/Reservas.java
@@ -78,7 +78,7 @@ public class Reservas implements Initializable {
     private boolean guest;
 
     ///////////////////parte de MisReservas//////////////////////////////////////////////////////////////////////////////////////////
-    public Button deleteButton;
+    public Button deleteButtonMR;
     public ListView<Booking> listView;
     public Button returnButton;
     private ObservableList<Booking> myObservableBookingList = null;
@@ -174,7 +174,7 @@ public class Reservas implements Initializable {
         listView.setCellFactory(c -> new bookingListCell());
 
         //in case of nothing selected disable delete
-        deleteButton.disableProperty().bind(
+        deleteButtonMR.disableProperty().bind(
                 Bindings.equal(-1,
                         listView.getSelectionModel().selectedIndexProperty()));
     }
@@ -449,11 +449,8 @@ public class Reservas implements Initializable {
     public void deleteAction(ActionEvent actionEvent) {
         if(!listView.getSelectionModel().getSelectedItem().isOlderForDay(LocalDate.now())){   //checks if booking was of a previous day
             JavaFXMLApplication.dialogBox("error","Cancel Error","Court was already used. Can't cancel reservation.");
-            return;
-        }
 
-        //check if canceled reservation is in less than 24h
-        if(Duration.between(listView.getSelectionModel().getSelectedItem().getBookingDate(), LocalDateTime.now()).compareTo(Duration.ofHours(24)) <= 0){
+        } else if(Duration.between(listView.getSelectionModel().getSelectedItem().getBookingDate(), LocalDateTime.now()).compareTo(Duration.ofHours(24)) < 0){
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Warning Dialog");
             alert.setHeaderText("Are you sure you want to continue?");
@@ -466,16 +463,17 @@ public class Reservas implements Initializable {
 
             alert.showAndWait().ifPresent(buttonType -> {
                 if(buttonType == buttonTypeYes) {
-                    System.out.println("Confirmed!");
+                    showConfirmationWindow();
                 } else if(buttonType == buttonTypeNo){
-                    System.out.println("Canceled.");
-                    return;
+                    JavaFXMLApplication.dialogBox("info","Action Canceled","Reservation deletion canceled.");
                 }
             });
+        } else {
+            showConfirmationWindow();
         }
 
 
-        showConfirmationWindow();
+
     }
 
     private void showConfirmationWindow(){
@@ -491,13 +489,14 @@ public class Reservas implements Initializable {
 
         alert.showAndWait().ifPresent(buttonType -> {
             if(buttonType == buttonTypeYes) {
-                System.out.println("Confirmed!");
+
                 myObservableBookingList.remove(listView.getSelectionModel().getSelectedItem());  //removes booking from listView
                 try {
                     JavaFXMLApplication.getCurrentClub().removeBooking(listView.getSelectionModel().getSelectedItem());  //removes booking from database
+                    JavaFXMLApplication.dialogBox("success","Process completed successfully","Your booking has been removed.");
                 }catch (ClubDAOException e){System.out.println("Error removing the booking.");}
             } else if(buttonType == buttonTypeNo){
-                System.out.println("Canceled.");
+                JavaFXMLApplication.dialogBox("info","Action Canceled","Reservation deletion canceled.");
             }
         });
     }
@@ -509,7 +508,7 @@ public class Reservas implements Initializable {
             if(empty || booking == null){
                 setText(null);
             }else{
-                setText( booking.getMadeForDay() + "          " +  booking.getCourt().getName() + "          " + booking.getFromTime() + " - " + booking.getFromTime().plusHours(1) + "          " + booking.getPaid());
+                setText( "       " + booking.getMadeForDay() + "                           " +  booking.getCourt().getName() + "                           " + booking.getFromTime() + " - " + booking.getFromTime().plusHours(1) + "                           " + booking.getPaid());
 
             }
 
